da09e03ab7f23d66160db79d49e85a66946b0559
Khaled Mohamed <km20161413@gmail.com>
2021-06-01 12:28:32 +0200

    "Mad Libs Generator" project
    This very simple project is about making a funny short story based on the user input

---
+++ Mad Libs Generator.py
@@ -0,0 +1,4 @@
+one = input("write an adjective: ")
+two = input("write a verb: ")
+three = input("write an adjective: ")
+print("Your birthday is a very " + one + " day! " "Birthdays are a way to " + two + " the day someone is born and to make them feel " + three)
